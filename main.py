f86e2ac63f76ae8ec40bca9e4a16e5fb701f45ae
Natã Andretto <nhandretto@gmail.com>
2024-10-26 10:08:54 -0300

    getpass

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import math
 import getpass
 
-senha = str(input('Senha:'))
-conf_senha = str(input('Confirme sua Senha:'))
+senha = getpass.getpass(input('Senha:'))
+conf_senha = getpass.getpass(input('Confirme sua Senha:'))
 
 if senha == conf_senha:
     print ('Senha salva')
 else:
     print ('Senhas não conferem')
+
 
