55b9a92c9ea6f33711ac9fb58042cf95a4b34496
Natã Andretto <nhandretto@gmail.com>
2024-10-26 10:02:59 -0300

    main code

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+import getpass
+
+senha = str(input('Senha:'))
+conf_senha = str(input('Confirme sua Senha:'))
+
+if senha = conf_senha
+    print ('Senha salva')
+else:
+    print ('Senhas não conferem')
+
