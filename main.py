026d8522fb59aa6fd83d5f42e7796ff41b995575
Natã Andretto <nhandretto@gmail.com>
2024-10-26 11:06:12 -0300

    Repeticao_de_caracteres

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-import math
 import getpass
 
-senha = getpass.getpass(input('Senha:'))
-conf_senha = getpass.getpass(input('Confirme sua Senha:'))
+def tem_sequencia(senha, tamanho_sequencia=3):
+    
+    # Cria um dicionário para contar a ocorrência de cada caractere
+    contador = {}
+    
+    for caractere in senha:
+        if caractere in contador:
+            contador[caractere] += 1
+        else:
+            contador[caractere] = 1
 
+    # Verifica se algum caractere tem uma contagem igual ou superior a tamanho_sequencia
+    for count in contador.values():
+        if count >= tamanho_sequencia:
+            return True
+            
+    return False
+
+# Solicitar senha do usuário
+senha = getpass.getpass('Senha: ')
+conf_senha = getpass.getpass('Confirme sua Senha: ')
+
+# Verificação de confirmação de senha
 if senha == conf_senha:
-    print ('Senha salva')
+    if tem_sequencia(senha):
+        print('A senha não pode ter uma sequência de caracteres repetidos, ex: aaa, 111, e etc.')
+    else:
+        print('Senha salva com sucesso!')
 else:
-    print ('Senhas não conferem')
-
+    print('As senhas não conferem.')
 
